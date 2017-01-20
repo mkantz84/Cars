33fc92144b8d6e0f690c4abdaad440c4c18518d4
kantz <kantz@LAPTOP-AE55S8JL>
2017-01-21 00:18:12 +0200

    fix validate

--- Cars/Code/ValidationUserId.cs
+++ Cars/Code/ValidationUserId.cs
@@ -12,30 +12,33 @@ namespace Cars.Code
 
         public override bool IsValid(object value)
         {
-            if (value==null)
+            try
+            {
+                string strID = value.ToString();
+                int[] id_12_digits = { 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+                int count = 0;
+
+                if (strID == null)
+                    return false;
+
+                strID = strID.PadLeft(9, '0');
+
+                for (int i = 0; i < 9; i++)
+                {
+                    int num = Int32.Parse(strID.Substring(i, 1)) * id_12_digits[i];
+
+                    if (num > 9)
+                        num = (num / 10) + (num % 10);
+
+                    count += num;
+                }
+
+                return (count % 10 == 0);
+            }
+            catch (Exception)
             {
                 return false;
             }
-            string strID = value.ToString();
-            int[] id_12_digits = { 1, 2, 1, 2, 1, 2, 1, 2, 1 };
-            int count = 0;
-
-            if (strID == null)
-                return false;
-
-            strID = strID.PadLeft(9, '0');
-
-            for (int i = 0; i < 9; i++)
-            {
-                int num = Int32.Parse(strID.Substring(i, 1)) * id_12_digits[i];
-
-                if (num > 9)
-                    num = (num / 10) + (num % 10);
-
-                count += num;
-            }
-
-            return (count % 10 == 0);
         }
     }
 }
